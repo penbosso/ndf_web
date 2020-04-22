3395b476c6b361fd505111bb15b8def94e20a9ed
penbosso <ckbosso@st.ug.edu.gh>
2020-04-22 15:21:37 +0000

    developed layout for site and cms

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { SiteHeaderComponent } from './layout/site-header/site-header.component';
+import { SiteFooterComponent } from './layout/site-footer/site-footer.component';
+import { SiteLayoutComponent } from './layout/site-layout/site-layout.component';
+import { CmsLayoutComponent } from './layout/cms-layout/cms-layout.component';
+import { CmsHeaderComponent } from './layout/cms-header/cms-header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    SiteHeaderComponent,
+    SiteFooterComponent,
+    SiteLayoutComponent,
+    CmsLayoutComponent,
+    CmsHeaderComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/layout/cms-header/cms-header.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cms-header',
+  templateUrl: './cms-header.component.html',
+  styleUrls: ['./cms-header.component.css']
+})
+export class CmsHeaderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
